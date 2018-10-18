a9e3c5595fd76ce1d2348df83bfa961d84775e02
sanjeev sharma <harleen.kaur@brihaspatitech.com>
2018-10-18 05:46:55 -0700

    Commit done

---
+++ ReusableProducts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReusableProducts
+//
+//  Created by Tbi-Pc-22 on 18/10/18.
+//  Copyright © 2018 Tbi-Pc-22. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
